8c0746a576ece9d0812b32cb3ba3cdd1868d9193
Adam Moujar Bakhti <amoujarbakhti@gmail.com>
2025-05-15 16:46:45 +0100

    Home page fix for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,19 @@ import Shop from './pages/Shop'
 import Product from './pages/Product'
 
 function App() {
-  return (
-    <CartProvider>
-    <div>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/product/:id" element={<Product />} />
-      </Routes>
-    </div>
-    </CartProvider>
-  )
+	return (
+		<CartProvider>
+			<div>
+				<Nav />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/Shopping-Cart-TOP/" element={<Home />} />
+					<Route path="/shop" element={<Shop />} />
+					<Route path="/product/:id" element={<Product />} />
+				</Routes>
+			</div>
+		</CartProvider>
+	)
 }
 
 export default App
